6f9187ecb7975f757fe07c086be3f5c0542191b6
Marciones <marcio.mr2@gmail.com>
2026-03-25 18:26:54 -0300

    fix: restaurar public supabase client

--- src/utils/supabase/public.ts
+++ src/utils/supabase/public.ts
@@ -1,11 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export function createPublicClient() {
-  return createServerSupabaseClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: { persistSession: false },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
